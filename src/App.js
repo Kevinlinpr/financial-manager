662dfb2cf5c72be3bb227775ccc674234e20b7ed
Kevinlinpr <kevinlinpr@gmail.com>
2020-04-20 16:12:49 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,276 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const unitStr = [
+  "人民币",
+  "美元"
+]
+const primaryColor = '#5c6bc0';
+const secondaryColor = '#304ffe';
+const borderColor = '#546e7a';
 function App() {
+  let [saveBtBk,setSaveBtBk] = useState(secondaryColor);
+  let [saveFtColor,setSaveFtColor] = useState('white');
+  let [editBtBk,setEditBtBk] = useState(primaryColor);
+  let [editFtColor,setEditFtColor] = useState('white');
+  let [edit,setEdit] = useState(true);
+  let [name,setName] = useState('');
+  let [found,setFound] = useState(0);
+  let [unit,setUnit] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width:'100vw',height:'100vh',display:'flex',alignItems:'center',flexDirection:'column'}}>
+      <div style={{width:'90vw',height:'300px',marginTop:'100px',backgroundColor:'white',border:'solid 3px '+borderColor,borderRadius:'10px'}}>
+        <div style={{width:'100%',height:'50px',backgroundColor:edit?secondaryColor:primaryColor,borderRadius:'6px 6px 0 0',display:'flex',alignItems:'center'}}>
+          <div>
+            <span className='web-font'>姓名：</span>
+            {
+              edit?<input className='web-input-font' type='text' style={{border:'solid 2px white',borderRadius:'5px',height:'28px',width:'150px'}} value={name} onChange={(e)=>{setName(e.currentTarget.value)}}/>:<span className='web-font'>{name}</span>
+            }
+          </div>
+          <div style={{marginLeft:'16px'}}>
+            <span className='web-font'>总资产：</span>
+            {
+              edit?<input className='web-input-font' type='text' style={{border:'solid 2px white',borderRadius:'5px',height:'28px',width:'150px'}} value={found} onChange={(e)=>{
+                setFound(e.currentTarget.value.replace(/[^\d]/g,'').replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,"))}}/>:<span className='web-font'>{found}</span>
+            }
+            {
+              edit?
+              <select  className='web-select-font' style={{marginLeft:'16px',border:'solid 2px white',borderRadius:'10px',width:'85px',height:'36px',backgroundColor:secondaryColor}}
+                onChange={(e)=>{
+                  let index = 0;
+                  switch(e.target.value){
+                    case unitStr[0]:
+                      index = 0;
+                      break;
+                    case unitStr[1]:
+                      index = 1;
+                      break;
+                    default:
+                      index = 0;
+                      break;
+                  }
+                  setUnit(index);
+                  console.log(index)
+                }}
+                defaultValue={unitStr[unit]}
+              >
+              <option id={0} className='web-select-font'>{unitStr[0]}</option>
+              <option id={1} className='web-select-font'>{unitStr[1]}</option>
+              </select>
+              :<span className='web-font'>{unitStr[unit]}</span>
+            }
+          </div>
+          <div style={{marginLeft:'auto',marginRight:'8px'}}>
+            
+            {
+              edit?<div style={{width:'80px',height:'36px',backgroundColor:saveBtBk,border:'solid 2px white',borderRadius:'5px',display:'flex',alignItems:'center',justifyContent:'center',cursor:'pointer'}}
+              onMouseEnter={()=>{
+                setSaveBtBk('white')
+                setSaveFtColor(secondaryColor);
+              }}
+              onMouseLeave={()=>{
+                setSaveBtBk(secondaryColor);
+                setSaveFtColor('white')
+              }}
+              onClick={()=>{
+                setEdit(false);
+              }}
+            >
+              <span className='web-font' style={{color:saveFtColor}}>保存</span>
+            </div>:
+              <div style={{width:'80px',height:'36px',backgroundColor:editBtBk,border:'solid 2px white',borderRadius:'5px',display:'flex',alignItems:'center',justifyContent:'center',cursor:'pointer'}}
+              onMouseEnter={()=>{
+                setEditBtBk('white')
+                setEditFtColor(primaryColor);
+              }}
+              onMouseLeave={()=>{
+                setEditBtBk(primaryColor)
+                setEditFtColor('white');
+              }}
+              onClick={()=>{
+                setEdit(true);
+              }}
+              >
+                <span className='web-font' style={{color:editFtColor}}>编辑</span>
+              </div>
+            }
+          
+          </div>
+        </div>
+        <div style={{width:'100%',height:'200px',display:'flex'}}>
+          <canvas id="myCanvas" width="200" height="200"></canvas>
+          <div style={{width:'30%',height:'100%',display:'flex',alignItems:'center'}}>
+            <div style={{width:'100px',height:'160px'}}>
+              <div style={{width:'100%',height:'25%',display:'flex',alignItems:'center'}}>
+                <div style={{width:'20px',height:'10px',backgroundColor:'#1cade4',marginRight:'8px'}}/>
+                <span className='sector-font'>保障金</span>
+              </div>
+              <div style={{width:'100%',height:'25%',display:'flex',alignItems:'center'}}>
+                <div style={{width:'20px',height:'10px',backgroundColor:'#2683c6',marginRight:'8px'}}/>
+                <span className='sector-font'>稳健增值</span>
+              </div>
+              <div style={{width:'100%',height:'25%',display:'flex',alignItems:'center'}}>
+                <div style={{width:'20px',height:'10px',backgroundColor:'#00b0ff',marginRight:'8px'}}/>
+                <span className='sector-font'>激进增值</span>
+              </div>
+              <div style={{width:'100%',height:'25%',display:'flex',alignItems:'center'}}>
+                <div style={{width:'20px',height:'10px',backgroundColor:'#1de9b6',marginRight:'8px'}}/>
+                <span className='sector-font'>日常支出</span>
+              </div>
+            </div>
+            <div style={{width:'100px',height:'160px'}}>
+              <div style={{width:'100%',height:'25%',display:'flex',alignItems:'center'}}>
+                <div style={{width:'15px',height:'15px',display:'flex',border:'solid 2px #546e7a',borderRadius:'5px',alignItems:'center',justifyContent:'center',cursor:'pointer'}}>
+                <span className='sector-font'>-</span>
+                </div>
+                <div style={{width:'45px',height:'15px',display:'flex',marginLeft:'8px',marginRight:'8px',justifyContent:'center'}}>
+                <span className='sector-font'>10</span><span className='sector-font'>%</span>
+                </div>
+                <div style={{width:'15px',height:'15px',display:'flex',border:'solid 2px #546e7a',borderRadius:'5px',alignItems:'center',justifyContent:'center',cursor:'pointer'}}>
+                <span className='sector-font'>+</span>
+                </div>
+              </div>
+              <div style={{width:'100%',height:'25%',display:'flex',alignItems:'center'}}>
+                <div style={{width:'15px',height:'15px',display:'flex',border:'solid 2px #546e7a',borderRadius:'5px',alignItems:'center',justifyContent:'center',cursor:'pointer'}}>
+                <span className='sector-font'>-</span>
+                </div>
+                <div style={{width:'45px',height:'15px',display:'flex',marginLeft:'8px',marginRight:'8px',justifyContent:'center'}}>
+                <span className='sector-font'>10</span><span className='sector-font'>%</span>
+                </div>
+                <div style={{width:'15px',height:'15px',display:'flex',border:'solid 2px #546e7a',borderRadius:'5px',alignItems:'center',justifyContent:'center',cursor:'pointer'}}>
+                <span className='sector-font'>+</span>
+                </div>
+              </div>
+              <div style={{width:'100%',height:'25%',display:'flex',alignItems:'center'}}>
+                <div style={{width:'15px',height:'15px',display:'flex',border:'solid 2px #546e7a',borderRadius:'5px',alignItems:'center',justifyContent:'center',cursor:'pointer'}}>
+                <span className='sector-font'>-</span>
+                </div>
+                <div style={{width:'45px',height:'15px',display:'flex',marginLeft:'8px',marginRight:'8px',justifyContent:'center'}}>
+                <span className='sector-font'>10</span><span className='sector-font'>%</span>
+                </div>
+                <div style={{width:'15px',height:'15px',display:'flex',border:'solid 2px #546e7a',borderRadius:'5px',alignItems:'center',justifyContent:'center',cursor:'pointer'}}>
+                <span className='sector-font'>+</span>
+                </div>
+              </div>
+              <div style={{width:'100%',height:'25%',display:'flex',alignItems:'center'}}>
+                <div style={{width:'15px',height:'15px',display:'flex',border:'solid 2px #546e7a',borderRadius:'5px',alignItems:'center',justifyContent:'center',cursor:'pointer'}}>
+                <span className='sector-font'>-</span>
+                </div>
+                <div style={{width:'45px',height:'15px',display:'flex',marginLeft:'8px',marginRight:'8px',justifyContent:'center'}}>
+                <span className='sector-font'>10</span><span className='sector-font'>%</span>
+                </div>
+                <div style={{width:'15px',height:'15px',display:'flex',border:'solid 2px #546e7a',borderRadius:'5px',alignItems:'center',justifyContent:'center',cursor:'pointer'}}>
+                <span className='sector-font'>+</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+window.addEventListener('load', eventWindowLoaded, false);
+
+function eventWindowLoaded () {
+  canvasApp(); //包含整个Canvas应用程序
+}
+function canvasSupport (e) {
+  
+	  return !!e.getContext;
+
+}
+function canvasApp () {
+  var myCanvas = document.getElementById('myCanvas');
+
+  if (!canvasSupport(myCanvas)) {
+	  return; 
+	}
+  
+  var ctx = myCanvas.getContext('2d');
+  
+  // 封装一个扇形函数
+  
+  // ctx: Canvas绘图环境
+  // x,y: 位移目标点
+  // r: 圆弧半径
+  // sDeg: 旋转起始角度
+  // eDeg: 旋转终点角度
+  
+  function drawSector(ctx, x, y,r, sDeg, eDeg) {
+    // 初始保存
+    ctx.save();
+    
+    //位移到目标点
+    ctx.translate(x, y);
+    ctx.beginPath();
+    
+    // 画出圆弧
+    ctx.arc(0, 0, r, sDeg, eDeg);
+    
+    // 再次保存以备旋转
+    ctx.save();
+    
+    // 旋转至起始角度
+    ctx.rotate(eDeg);
+    
+    // 移动到终点，准备连接终点与圆心
+    ctx.moveTo(r, 0);
+    
+    // 连接到圆心
+    ctx.lineTo(0, 0);
+    
+    // 还原
+    ctx.restore();
+    
+    // 旋转至起点角度
+    ctx.rotate(sDeg);
+    
+    // 从圆心连接到起点
+    ctx.lineTo(r, 0);
+    
+    ctx.closePath();
+    // 还原到最初保存的状态
+    ctx.restore();
+  }
+  
+  function drawScreen () {
+    
+    var deg = Math.PI / 180;
+    
+    var obj = {
+      x: 100,
+      y: 100,
+      r: 80,
+      sDeg: [-90,-90,120,180,240],
+      eDeg: [-90,120,180,240,270],
+      style: ['#1cade4','#2683c6','#00b0ff','#1de9b6']
+    }
+
+    var obj2 = {
+      x: 100,
+      y: 100,
+      r: 30,
+      sDeg: [-90,-90],
+      eDeg: [-90,270],
+      style: ['#eceff1']
+    }
+    
+    for (var i = 0; i < obj.sDeg.length; i++) {
+      drawSector(ctx,obj.x, obj.y, obj.r, obj.sDeg[i] * deg, obj.eDeg[i] * deg);
+      ctx.fill();
+      ctx.fillStyle = obj.style[i];
+    }
+    for (var i = 0; i < obj2.sDeg.length; i++) {
+      drawSector(ctx,obj2.x, obj2.y, obj2.r, obj2.sDeg[i] * deg, obj2.eDeg[i] * deg);
+      ctx.fill();
+      ctx.fillStyle = obj2.style[i];
+    }
+  }
+  
+  drawScreen();
+  
+}
