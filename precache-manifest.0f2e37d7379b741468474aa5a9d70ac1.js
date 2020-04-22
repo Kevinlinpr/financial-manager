4969584e7b73b482fc15b1a89ccbb1c792f459ba
Kevinlinpr <kevinlinpr@gmail.com>
2020-04-23 03:47:02 +0800

    Updates

---
+++ precache-manifest.0f2e37d7379b741468474aa5a9d70ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb9a80dc207504cdcb2187b7d65680d",
+    "url": "/financial-manager/index.html"
+  },
+  {
+    "revision": "33c505ac34132d55fec7",
+    "url": "/financial-manager/static/css/main.aeaa9391.chunk.css"
+  },
+  {
+    "revision": "31be13b19a999a24c2a4",
+    "url": "/financial-manager/static/js/2.46d74c26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/financial-manager/static/js/2.46d74c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c505ac34132d55fec7",
+    "url": "/financial-manager/static/js/main.a5825092.chunk.js"
+  },
+  {
+    "revision": "8090bc434e425c5ad7d7",
+    "url": "/financial-manager/static/js/runtime-main.63075620.js"
+  }
+]);
